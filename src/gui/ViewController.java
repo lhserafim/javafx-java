5a65dc24f0d4444b37f404dd64cd84ba587cc965
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2020-05-24 23:23:57 -0300

    Implementação da classe Constraints, para colocar restrições no tetfield

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,15 +1,20 @@
 package gui;
 
 import gui.util.Alerts;
+import gui.util.Constraints;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.net.URL;
 import java.util.Locale;
+import java.util.ResourceBundle;
 
-public class ViewController {
+// Para implementar o controller, necessário implementar o Initializable e seu método
+public class ViewController implements Initializable {
 
     // Para fazer as referencias entre tela
     // Sempre colocar a notação @FXML para referenciar atributos de tela
@@ -32,8 +37,19 @@ public class ViewController {
             labelResult.setText(String.format("%.2f",sum));
         }
         catch (NumberFormatException e) {
-            Alerts.showAlert("Error","Error","Invalid number", Alert.AlertType.ERROR);
+            Alerts.showAlert("Error","Error",e.getMessage(), Alert.AlertType.ERROR);
         }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // colocar ações que eu quero sejam executadas no momento da instanciação do controlador
+        Constraints.setTextFieldDouble(txtNumber1);
+        Constraints.setTextFieldDouble(txtNumber2);
+        //Constraints.setTextFieldInteger(txtNumber1);
+        //Constraints.setTextFieldInteger(txtNumber2);
+        Constraints.setTextFieldMaxLength(txtNumber1,12);
+        Constraints.setTextFieldMaxLength(txtNumber2,12);
     }
 
     /*
