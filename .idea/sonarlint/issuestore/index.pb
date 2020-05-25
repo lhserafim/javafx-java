4d76868c8ee15c375c2ed58c1e4adfd060b129d0
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2020-05-24 23:04:16 -0300

    Implementado soma de itens via tela

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+I
+src/application/Main.java,8/d/8dd39b27bbecf58d30bde4cb59516e5bb7832dd9
+A
+src/gui/View.fxml,8/b/8b55d74c11ba577acc9d3e83b57e3cf9d41a04ea
+H
+src/gui/util/Alerts.java,4/0/40d5824781686750c8b4ecdf9cfa389033482a4e
+K
+src/gui/ViewController.java,2/a/2a26acc16ddb2e855aed879e96370d7c842302d6
--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,20 +1,39 @@
 package gui;
 
-import gui.util.Alerts;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.util.Locale;
 
 public class ViewController {
 
     // Para fazer as referencias entre tela
     // Sempre colocar a notação @FXML para referenciar atributos de tela
     @FXML
-    private Button btTest; //javafx.scene.control.Button;
+    private TextField txtNumber1;
+    @FXML
+    private TextField txtNumber2;
+    @FXML
+    private Label labelResult;
+    @FXML
+    private Button btSum; //javafx.scene.control.Button;
 
+    @FXML
+    public void onBtSumAction() {
+        Locale.setDefault(Locale.US);
+        double number1 = Double.parseDouble(txtNumber1.getText());
+        double number2 = Double.parseDouble(txtNumber2.getText());
+        double sum = number1 + number2;
+        labelResult.setText(String.format("%.2f",sum));
+    }
+
+    /*
     @FXML
     public void onBtTestAction() {
         //System.out.println("Click");
         Alerts.showAlert("Alert Title","Alert Header","Alert Content", Alert.AlertType.INFORMATION);
     }
+     */
 }
